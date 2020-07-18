96853b34460953806cb352402a40d17ad8f1023b
sirsuccess <kanuamani@gmail.com>
2020-07-18 17:20:06 +0100

    implement search feature

---
+++ src/components/search.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { groupBy } from "../utils";
+
+function SearchComponent({ getData, episodes, history }) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formElements = e.target.elements;
+    let payload;
+
+    Array.prototype.forEach.call(formElements, (element) => {
+      payload = element.value;
+    });
+    getData(payload);
+  };
+
+  const handleRedirect = (season) =>
+    history.push({ pathname: "/season", state: season });
+
+  const season = Object.entries(groupBy(episodes, "season"));
+
+  return (
+    <div className="search">
+      <form name="search" onSubmit={handleSubmit}>
+        <div className="innerSearch">
+          <input
+            autofocus="autofocus"
+            type="text"
+            name="item"
+            placeholder="Search movie..."
+
+            // onChange={searchFunct}
+          ></input>
+          {/* <button type="reset" className="btn">
+            X
+          </button> */}
+        </div>
+      </form>
+      {season.map((movie, index) => {
+        return (
+          <li onClick={() => handleRedirect(index)}>
+            <div className="searchImg">
+              <img src={movie[1][0].image.medium} alt="movie poster" />
+            </div>
+            <div className="search-item">
+              <div>{movie[1][0].name}</div>
+              <div className="search-season">Season {index + 1}</div>
+            </div>
+          </li>
+        );
+      })}
+    </div>
+  );
+}
+export default withRouter(SearchComponent);
