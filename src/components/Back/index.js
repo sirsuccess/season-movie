b2cce065b6a679de4044381b132f9054757d65d5
sirsuccess <kanuamani@gmail.com>
2020-07-18 17:23:35 +0100

    created back button

---
+++ src/components/Back/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Back = () => {
+  return (
+    <button
+      onClick={() => {
+        window.history.back();
+      }}
+      style={{
+        width:"60px",
+        fontSize: "16px",
+        lineHeight: "18px",
+        color: "#58595B",
+        border:"1px solid gray",
+        outline: "none",
+        backgroundColor: "white",
+        cursor: "pointer",
+        display: "flex",
+        borderRadius:"5px",
+        "justify-content": "space-between",
+        alignItems:"center",
+        marginBottom: "1em" 
+    }}
+    >
+      <img
+        src={require("../../assets/icons/back-arrow.svg")}
+        alt="back arrow"
+        
+      />
+      Back
+    </button>
+  );
+};
+
+export default Back;
